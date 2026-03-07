a2425ca58b91c03361b7109e937fa1591c64951b
TAN SWEE XIN <25070762@siswa.um.edu.my>
2026-03-07 15:12:48 +0800

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){ //default cart when cart is empty
+  cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId){
@@ -22,6 +31,7 @@ export function addToCart(productId){
         productId: productId,
         quantity: 1
       });
+      saveToStorage();
     }
 }
 
@@ -33,4 +43,5 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart;
+  saveToStorage();
 }
