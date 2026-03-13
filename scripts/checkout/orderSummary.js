86670a22f741cb93fd4ca06491cc51dad4cb22d7
TAN SWEE XIN <25070762@siswa.um.edu.my>
2026-03-14 00:29:04 +0800

    convert products into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -41,7 +41,7 @@ export function renderOrderSummary(){
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            RM${formatCurrency(matchingProduct.priceCents)}
+            RM${matchingProduct.getPrice()}
           </div>
           <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
